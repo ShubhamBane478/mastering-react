ca81c9a3fba0f35f98fbc3c42ecd1f464d9b4e98
Shubham Bane <74781889+ShubhamBane478@users.noreply.github.com>
2023-11-27 05:33:33 +0000

    Half Passgenrator Completed

--- src/mini_projects/PassGenratorApp.jsx
+++ src/mini_projects/PassGenratorApp.jsx
@@ -1,9 +1,23 @@
-import {useState} from 'react'
+import {useState , useCallback} from 'react'
 
 function PassGenratorApp(){
+
+    const [ length , setLength] = useState(8)
+    const [numberAllowed, setNumberAllowed] = useState(true)
+    const [characterAllowed , setCharacterAllowed] =  useState("")
+    const [Password , serPassword] = useState()
+
+    
+
+    const passwordGenerator = useCallback(()=> {
+
+    }, [length, numberAllowed , characterAllowed, serPassword])
     return(
         <>
-        <h1>Shubham Bane</h1>
+        <div className='bg-black w-full min-h-screen'>
+            <h1 className='text-white'>Password Generator</h1>
+        </div>
+        
         </>
     )
 }
