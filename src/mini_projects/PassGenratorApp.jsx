5be6dd1767feb747ca11b21f6451cb9c21922e3a
Shubham Bane <74781889+ShubhamBane478@users.noreply.github.com>
2023-11-29 05:39:57 +0000

    70% Completed PassGenrator

--- src/mini_projects/PassGenratorApp.jsx
+++ src/mini_projects/PassGenratorApp.jsx
@@ -5,17 +5,57 @@ function PassGenratorApp(){
     const [ length , setLength] = useState(8)
     const [numberAllowed, setNumberAllowed] = useState(true)
     const [characterAllowed , setCharacterAllowed] =  useState("")
-    const [Password , serPassword] = useState()
+    const [password , setPassword] = useState()
 
     
 
     const passwordGenerator = useCallback(()=> {
+        let pass=""
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        
+        if( numberAllowed) str+= "01234565789"
+        if( numberAllowed) str+= "!@#$%^&*-_+=[]{}~`"
 
-    }, [length, numberAllowed , characterAllowed, serPassword])
+        for(let i = 1 ; i<= Array.length ; i++){
+            let char = Math.floor(Math.random() * str.length + 1)
+            pass = str.charAt(char)
+        }
+
+        setPassword(pass)
+
+
+    }, [length, numberAllowed , characterAllowed, setPassword])
+
+
     return(
         <>
-        <div className='bg-black w-full min-h-screen'>
-            <h1 className='text-white'>Password Generator</h1>
+        <div className='bg-black w-full  h-screen	pt-8'>
+            <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 mb-8 bg-gray-800 text-orange-500">
+                <h1 className='text-white text-center my-3'>Password generator</h1>
+                    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+                        <input
+                            type='text'
+                            value={password}
+                            className='outline-none w-full py-1 px-3'
+                            placeholder='Password'
+                            readOnly
+                            />
+                        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+                    </div>
+                    <div className='flex text-sm gap-x-2'>
+                        <div className='flex items-center gap-x-1'>
+                            <input
+                            type='range'
+                            min={0}
+                            max={100}
+                            value={length}
+                            className='cursor-pointer' 
+                            onChange={(e) => {setLength(e.target.value)}}/>
+
+                        </div>
+                    </div>
+                        
+            </div>
         </div>
         
         </>
