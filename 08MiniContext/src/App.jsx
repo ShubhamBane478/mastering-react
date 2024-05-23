ebf8fd36ca97c78672b1a0cc6a33609714679f75
Shubham Bane <74781889+ShubhamBane478@users.noreply.github.com>
2024-05-23 06:15:55 +0000

    Style the Context API form

--- 08MiniContext/src/App.jsx
+++ 08MiniContext/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   
   return (
     <UserContextProvider>
-      <h3>Context API </h3>
       <Login/>
       <Profile/>
     </UserContextProvider>
--- 08MiniContext/src/components/Profile.jsx
+++ 08MiniContext/src/components/Profile.jsx
@@ -5,9 +5,9 @@ const Profile = () => {
 
     const {user} = useContext(UserContext);
     
-    if(!user) return <div>Please Login</div>
+    if(!user) return <div className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Please Login</div>
     
-    return <div>WelCome {user.username} and you password is {user.password}</div>
+    return <div className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Welcome {user.username} and you password is {user.password}</div>
        
     
  
