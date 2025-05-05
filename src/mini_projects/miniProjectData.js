8dda5ac9964ac5b11e1240a1e96b936134d98daf
ShbhamBane478 <shubhambane435@gmail.com>
2025-05-05 21:27:22 +0530

    Tic tac toe level 1

--- src/mini_projects/miniProjectData.js
+++ src/mini_projects/miniProjectData.js
@@ -1,11 +1,11 @@
-import ticTacToe from "./ticTacToe/ticTacToe";
+import TicTacToe from "./ticTacToe/ticTacToe";
 
 const miniProjectData = [ {
     id: 1 , 
     title : "Tic Tac Toe Game",
     description : "Basic game creation to understand the working on react",
     projectName : "tic-tac-toe",
-    projectComponent : ticTacToe,
+    projectComponent : TicTacToe,
 },
 ]
 
